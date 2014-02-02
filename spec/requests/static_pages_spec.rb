2cdd17853187e960b8025e7df11c3401b656339d
Masami Nasu <masami.nasu@gmail.com>
2014-02-02 18:11:16 +0900

    refactor 2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,54 +6,29 @@ describe "Static pages" do
   describe "Home page" do
   	before {visit root_path}
 
-    it "should have the h1 'Sample App'" do
-      should have_selector('h1', :text => 'Sample App')
-    end
-
-    it "should have the title " do
-      should have_selector('title',
-                        text: full_title(''))
-    end
-
-    it "should not have a custom page title" do
-    	should_not have_selector('title',text:'|Home')
-    end
-
+    it{should have_selector('h1', :text => 'Sample App')}
+    it{should have_selector('title',text: full_title(''))}
+    it{should_not have_selector('title',text:'|Home')}
   end
 
   describe "Help page" do
   	before {visit help_path}
 
-    it "should have the h1 'Help'" do
-      should have_selector('h1', :text => 'Help')
-    end
-
-    it "should have the title 'Help'" do
-      should have_selector('title', text: full_title('Help'))
-    end
+    it{should have_selector('h1', :text => 'Help')}
+    it{should have_selector('title', text: full_title('Help'))}
   end
 
   describe "About page" do
   	before {visit about_path}
 
-    it "should have the h1 'About Us'" do
-      should have_selector('h1', :text => 'About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      should have_selector('title', text: full_title("About Us"))
-    end
+    it{should have_selector('h1', :text => 'About Us')}
+    it{should have_selector('title', text: full_title("About Us"))}
   end
 
   describe "Contact page" do
   	before {visit contact_path}
 
-  	it "should have the h1 'Contact'" do
-  		should have_selector('h1', text: 'Contact');
-  	end
-
-  	it "should have the title 'Contact'" do 
-  		should have_selector('title',text: full_title("Contact"))
-  	end
+  	it{should have_selector('h1', text: 'Contact')}
+  	it{should have_selector('title',text: full_title("Contact"))}
   end
 end
